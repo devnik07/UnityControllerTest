77f051dc555acbb5e1c95d2cdcaffc7cafff2f3d
Niklas Oswald <niklas.oswald20@web.de>
2025-04-02 13:22:09 +0200

    implemented zooming using joystick inputs

--- Assets/My Scripts/ControllerTracker.cs
+++ Assets/My Scripts/ControllerTracker.cs
@@ -4,11 +4,23 @@ using Valve.VR;
 public class ControllerTracker : MonoBehaviour
 {
     public SteamVR_Action_Pose poseAction;
+    public SteamVR_Action_Boolean toggleZoomAction;
+    public SteamVR_Action_Vector2 joystickAction;
+    public float zoomSpeed = 0.04f;
+
+    private bool zoomModeOn;
+    private float zoom;
+    private float toggleTimeout = 1.0f;
+    private float lastToggleTime;
+
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        zoom = 1.0f;
+        zoomModeOn = false;
+        lastToggleTime = 0f;
     }
 
     // Update is called once per frame
@@ -20,6 +32,32 @@ public class ControllerTracker : MonoBehaviour
         //(q.z, q.x) = (q.x, q.z);
         transform.rotation = q;
         transform.Rotate(90.0f, 0, 0);
+
+        position.x -= 1.5f;
+        position.y += 0.75f;
         transform.position = position;
+
+        bool toggleZoom = toggleZoomAction.GetState(SteamVR_Input_Sources.Any);
+        if (Time.time - lastToggleTime >= toggleTimeout) {
+            if (zoomModeOn && toggleZoom) {
+                zoomModeOn = false;
+                lastToggleTime = Time.time;
+                print("ControllerTracker: Zoom mode deactivated");
+            } else if (toggleZoom) {
+                zoomModeOn = true;
+                lastToggleTime = Time.time;
+                print("ControllerTracker: Zoom mode activated");
+            }
+        }
+
+        if (zoomModeOn) {
+            float zoomInput = joystickAction.GetAxis(SteamVR_Input_Sources.Any).y;
+            if (Mathf.Abs(zoomInput) > 0.1f) {
+                zoom += zoomInput * zoomSpeed;
+                Vector3 newScale = new(zoom, zoom, zoom);
+                transform.localScale = newScale;
+                print(transform.localScale);
+            }
+        }
     }
 }
---
+++ Assets/My Scripts/MoveCamera.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using Valve.VR;
+
+public class MoveCamera : MonoBehaviour
+{
+    public SteamVR_Action_Boolean toggleZoomAction;
+    public SteamVR_Action_Vector2 joystickAction;
+    private float movementSpeed = 2f;
+    private float rotationSpeed = 120.0f;
+    private bool zoomModeOn;
+    private float toggleTimeout = 1.0f;
+    private float lastToggleTime;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        zoomModeOn = false;
+        lastToggleTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool toggleZoom = toggleZoomAction.GetState(SteamVR_Input_Sources.Any);
+        if (Time.time - lastToggleTime >= toggleTimeout) {
+            if (zoomModeOn && toggleZoom) {
+                zoomModeOn = false;
+                lastToggleTime = Time.time;
+                print("MoveCamera: Zoom mode deactivated");
+            } else if (toggleZoom) {
+                zoomModeOn = true;
+                lastToggleTime = Time.time;
+                print("MoveCamera: Zoom mode activated");
+            }
+        }
+
+        if (!zoomModeOn) {
+            Vector2 joystickInput = joystickAction.GetAxis(SteamVR_Input_Sources.Any);
+            if (Mathf.Abs(joystickInput.x) <= 0.1f) {
+                joystickInput.x = 0;
+            }
+            if (Mathf.Abs(joystickInput.y) <= 0.1f) {
+                joystickInput.y = 0;
+            }
+
+            /** Unity's x and z direction corresponds to the OpenVR's -y and x axis respectively */
+            //Vector3 movementDirection = transform.right * (-1) * joystickInput.y + transform.forward * joystickInput.x;
+            //movementDirection.y = 0;
+            //transform.position += movementDirection * movementSpeed * Time.deltaTime;
+            
+            Vector3 moveStraight = transform.right * (-1) * joystickInput.y;
+            transform.position += moveStraight * movementSpeed * Time.deltaTime;
+
+            float turn = joystickInput.x * rotationSpeed * Time.deltaTime;
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+            transform.rotation *= turnRotation;
+        }
+    }
+}
